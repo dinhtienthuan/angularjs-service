2d9594cae4589199015d9885ed30ba0f96868435
Thuan T. DINH <dinhtienthuan@gmail.com>
2016-08-19 00:45:10 +0700

    Using URL Routing: Defining the URL Routes

--- products.js
+++ products.js
@@ -1,5 +1,25 @@
 angular.module("exampleApp", ["increment", "ngResource"])
-  .constant("baseUrl", "http://localhost:5500/products/")
+  .constant("baseUrl", "http://localhost:5500/products/")route
+  .config(function($routeProvider, $locationProvider) {
+    
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when("/list", {
+      templateUrl: "tableView.html"
+    });
+
+    $routeProvider.when("edit", {
+      templateUrl: "/editorView.html"
+    });
+
+    $routeProvider.when("create", {
+      templateUrl: "/editorView.html"
+    });
+
+    $routeProvider.otherwise({
+      templateUrl: "/tableView.html"
+    });
+  })
   .controller("defaultCtrl", function($scope, $http, $resource, baseUrl) {
     
     $scope.displayMode = "list";
